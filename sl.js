8c8dd1cd4b734c3998057d5d23cb9fbe5c5ec4fd
Alexander Steen <alexander.steen@mynt.se>
2025-09-19 14:13:27 +0200

    First commit

---
+++ sl.js
@@ -0,0 +1,15 @@
+async function getDepartures(stop, ...destinations) {
+    const url = `https://transport.integration.sl.se/v1/sites/${stop}/departures`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        console.log(`Response status: ${response.status}`);
+        return "Kunde inte hämta data";
+    }
+    const result = await response.json();
+    console.log(result);
+    const filtered = result.departures
+    .filter(d => destinations.includes(d.destination))
+    .map(d => d.line.designation + " mot " + d.destination + " - " + d.display + "\n");
+      
+    return filtered;
+}
